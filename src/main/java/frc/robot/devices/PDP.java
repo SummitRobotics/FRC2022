6de3c0879c2ca0e870dfc4f4c28822d042bf991c
Owen McCormick <oweninoregon@gmail.com>
2022-01-19 16:16:08 -0800

    Created PDP.java and wrote simple methods

---
+++ src/main/java/frc/robot/devices/PDP.java
@@ -0,0 +1,52 @@
+package frc.robot.devices;
+
+import edu.wpi.first.wpilibj.PowerDistribution;
+
+public class PDP {
+
+    private PowerDistribution panel;
+
+    public PDP() {
+        panel = new PowerDistribution();
+    }
+
+    public void clearStickyFaults() {
+        panel.clearStickyFaults();
+    }
+    
+    public void close() {
+        panel.close();
+    }
+
+    public double getCurrent(int channel) {
+        return panel.getCurrent(channel);
+    }
+    
+    public int getModule() {
+        return panel.getModule();
+    }
+
+    public boolean getSwitchableChannel() {
+        return panel.getSwitchableChannel();
+    }
+
+    public double getTemperature() {
+        return panel.getTemperature();
+    }
+
+    public double getTotalCurrent() {
+        return panel.getTotalCurrent();
+    }
+
+    public double getTotalEnergy() {
+        return panel.getTotalEnergy();
+    }
+
+    public double getTotalPower() {
+        return panel.getTotalPower();
+    }
+
+    public double getVoltage() {
+        return panel.getVoltage();
+    }
+}
