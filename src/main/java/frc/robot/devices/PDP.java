64705f1641b6b48da01008d7f1b7f824bde6e0a2
Xmde <erve2005@gmail.com>
2022-01-21 22:47:11 -0800

    Reformat code

--- src/main/java/frc/robot/devices/PDP.java
+++ src/main/java/frc/robot/devices/PDP.java
@@ -10,63 +10,63 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class PDP implements Sendable {
 
-    private PowerDistribution panel;
+  private PowerDistribution panel;
 
-    public PDP() {
-        panel = new PowerDistribution();
-        SmartDashboard.putData("PDP", this);
-    }
+  public PDP() {
+    panel = new PowerDistribution();
+    SmartDashboard.putData("PDP", this);
+  }
 
-    public void clearStickyFaults() {
-        panel.clearStickyFaults();
-    }
-    
-    public void close() {
-        panel.close();
-    }
+  public void clearStickyFaults() {
+    panel.clearStickyFaults();
+  }
 
-    public double getCurrent(int channel) {
-        return panel.getCurrent(channel);
-    }
+  public void close() {
+    panel.close();
+  }
 
-    public boolean getSwitchableChannel() {
-        return panel.getSwitchableChannel();
-    }
+  public double getCurrent(int channel) {
+    return panel.getCurrent(channel);
+  }
 
-    public double getTemperature() {
-        return panel.getTemperature();
-    }
+  public boolean getSwitchableChannel() {
+    return panel.getSwitchableChannel();
+  }
 
-    public double getTotalCurrent() {
-        return panel.getTotalCurrent();
-    }
+  public double getTemperature() {
+    return panel.getTemperature();
+  }
 
-    public double getTotalEnergy() {
-        return panel.getTotalEnergy();
-    }
+  public double getTotalCurrent() {
+    return panel.getTotalCurrent();
+  }
 
-    public double getTotalPower() {
-        return panel.getTotalPower();
-    }
+  public double getTotalEnergy() {
+    return panel.getTotalEnergy();
+  }
 
-    public double getVoltage() {
-        return panel.getVoltage();
-    }
+  public double getTotalPower() {
+    return panel.getTotalPower();
+  }
 
-    public void resetTotalEnergy() {
-        panel.resetTotalEnergy();
-    }
+  public double getVoltage() {
+    return panel.getVoltage();
+  }
 
-    public void toggleSwitchableChannel() {
-        panel.setSwitchableChannel(!panel.getSwitchableChannel());
-    }
+  public void resetTotalEnergy() {
+    panel.resetTotalEnergy();
+  }
 
-    @Override
-    public void initSendable(SendableBuilder builder) {
-        builder.setSmartDashboardType("PDP");
-        builder.addDoubleProperty("temperature", this::getTemperature, null);
-        builder.addDoubleProperty("total current", this::getTotalCurrent, null);
-        builder.addDoubleProperty("voltage", this::getVoltage, null);
+  public void toggleSwitchableChannel() {
+    panel.setSwitchableChannel(!panel.getSwitchableChannel());
+  }
 
-    }
+  @Override
+  public void initSendable(SendableBuilder builder) {
+    builder.setSmartDashboardType("PDP");
+    builder.addDoubleProperty("temperature", this::getTemperature, null);
+    builder.addDoubleProperty("total current", this::getTotalCurrent, null);
+    builder.addDoubleProperty("voltage", this::getVoltage, null);
+
+  }
 }
