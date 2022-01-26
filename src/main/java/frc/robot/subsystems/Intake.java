8c76ee796862030a9afb2fe70cd30d0307204eda
Owen McCormick <oweninoregon@gmail.com>
2022-01-26 15:29:14 -0800

    Added intake.java methods

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -6,9 +6,12 @@ import com.revrobotics.RelativeEncoder;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utilities.ChangeRateLimiter;
 import frc.robot.utilities.lists.Ports;
 
 public class Intake extends SubsystemBase {
+
+    public static final double INTAKE_RATE = 0.01;
 
     // motor
     private final CANSparkMax intakeMotor =
@@ -21,6 +24,55 @@ public class Intake extends SubsystemBase {
     // encoder
     private final RelativeEncoder intakeEncoder = intakeMotor.getEncoder();
 
-    public Intake() {}
-    
+    // rate limiter
+    private final ChangeRateLimiter intakeRateLimiter = new ChangeRateLimiter(INTAKE_RATE);
+
+    private boolean intakeSolenoidPosition = false;
+
+
+    public Intake() {
+        zeroEncoder();
+    }
+
+    public void setIntakeMotorPower(double power) {
+        intakeMotor.set(intakeRateLimiter.getRateLimitedValue(power));
+    }
+
+    public double getIntakeEncoderPosition() {
+        return intakeEncoder.getPosition();
+    }
+
+    public double getIntakeRPM() {
+        return intakeEncoder.getVelocity();
+    }
+
+    public void setIntakeEncoder(double position) {
+        intakeEncoder.setPosition(position);
+    }
+
+    public void zeroEncoder() {
+        setIntakeEncoder(0);
+    }
+
+    public void stop() {
+        intakeMotor.stopMotor();
+    }
+
+    public void resetRateLimiter() {
+        intakeRateLimiter.resetOld();
+    }
+
+    public void toggleIntakeSolenoid() {
+        intakeSolenoidPosition = !intakeSolenoidPosition;
+        intakeSolenoid.toggle();
+    }
+
+    public void setIntakeSolenoid(boolean value) {
+        intakeSolenoidPosition = value;
+        intakeSolenoid.set(value);
+    }
+
+    public boolean getIntakeSolenoid() {
+        return intakeSolenoidPosition;
+    }
 }
