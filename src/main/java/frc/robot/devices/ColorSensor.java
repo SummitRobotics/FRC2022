f4426b022a049dadc0df57766e61a26591f43617
Owen McCormick <oweninoregon@gmail.com>
2022-01-29 12:25:16 -0800

    Added telemetry and javadocs for ColorSensor.java

--- src/main/java/frc/robot/devices/ColorSensor.java
+++ src/main/java/frc/robot/devices/ColorSensor.java
@@ -33,34 +33,66 @@ public class ColorSensor implements Sendable {
         colorMatcher.addColorMatch(redTarget);
     }
 
+    /**
+     * Gets the red value detected by the sensor.
+     *
+     * @return red the red value detected by the sensor
+     */
     public double getRed() {
         return colorSensor.getColor().red;
     }
 
+    /**
+     * Gets the green value detected by the sensor.
+     *
+     * @return green the green value detected by the sensor
+     */
     public double getGreen() {
         return colorSensor.getColor().green;
     }
 
+    /**
+     * Gets the blue value detected by the sensor.
+     *
+     * @return blue the blue value detected by the sensor
+     */
     public double getBlue() {
         return colorSensor.getColor().blue;
     }
 
-    public ColorMatchResult getColorMatchResult() {
+    /**
+     * Returns the ColorMatchResult containing the sensor's data.
+     * This is probably not what you want to use.
+     *
+     * @return colorMatch the ColorMatchResult containing the sensor's data.
+     */
+    public ColorMatchResult getColorMatch() {
         return colorMatcher.matchClosestColor(colorSensor.getColor());
     }
 
-    public String checkColorString() {
-        if (getColorMatchResult().color == blueTarget) {
+    /**
+     * Returns the name of the color detected.
+     *
+     * @return color_name the name of the color detected
+     */
+    public String getColorString() {
+        if (getColorMatch().color == blueTarget) {
             return "Blue";
-        } else if (getColorMatchResult().color == redTarget) {
+        } else if (getColorMatch().color == redTarget) {
             return "Red";
         } else {
             return "Unknown";
         }
     }
 
+    /**
+     * Returns the level of confidence in the color selection.
+     * Higher numbers mean higher confidence.
+     *
+     * @return confidence the level of confidence in the color selection
+     */
     public double getConfidence() {
-        return 1 - getColorMatchResult().confidence;
+        return 1 - getColorMatch().confidence;
     }
 
 
@@ -70,5 +102,7 @@ public class ColorSensor implements Sendable {
         builder.addDoubleProperty("red", this::getRed, null);
         builder.addDoubleProperty("green", this::getGreen, null);
         builder.addDoubleProperty("blue", this::getBlue, null);
+        builder.addStringProperty("detected_color", this::getColorString, null);
+        builder.addDoubleProperty("confidence", this::getConfidence, null);
     }
 }
