7b1b826573b9346f5342f6c5cc86d4e0c9a368d9
Owen McCormick <oweninoregon@gmail.com>
2022-01-29 15:33:50 -0800

    Finished conveyor state reporting

--- src/main/java/frc/robot/commands/conveyor/ConveyorAutomation.java
+++ src/main/java/frc/robot/commands/conveyor/ConveyorAutomation.java
@@ -20,11 +20,18 @@ public class ConveyorAutomation extends CommandBase {
     
     private final ColorSensor colorSensor;
     private final LidarV3 lidar;
-    private final ConveyorState frontState;
-    private final ConveyorState backState;
+    private ConveyorState frontState;
+    private ConveyorState backState;
+    private String previousColorSensorMeasurement;
+    private String colorSensorMeasurement;
+    private double lidarDistance;
+
+    private static final double
+        MIN_LIDAR_DISTANCE = 0,
+        MAX_LIDAR_DISTANCE = 0;
 
     /**
-     * Command to track the possibe states of positions within the conveyor.
+     * Command to track the possible states of positions within the conveyor.
      *
      * @param colorSensor the color sensor
      * @param lidar the lidar
@@ -32,9 +39,12 @@ public class ConveyorAutomation extends CommandBase {
     public ConveyorAutomation(ColorSensor colorSensor, LidarV3 lidar) {
         this.colorSensor = colorSensor;
         this.lidar = lidar;
+
         frontState = ConveyorState.NONE;
         backState = ConveyorState.NONE;
-
+        previousColorSensorMeasurement = "Unknown";
+        colorSensorMeasurement = "Unknown";
+        lidarDistance = -1.0;
     }
 
     @Override
@@ -44,7 +54,23 @@ public class ConveyorAutomation extends CommandBase {
 
     @Override
     public void execute() {
+        previousColorSensorMeasurement = colorSensorMeasurement;
+        colorSensorMeasurement = colorSensor.getColorString();
+        lidarDistance = lidar.getDistance();
 
+        if (colorSensorMeasurement != previousColorSensorMeasurement) {
+            if (MIN_LIDAR_DISTANCE <= lidarDistance && lidarDistance <= MAX_LIDAR_DISTANCE) {
+                backState = frontState;
+
+                if (colorSensor.getColorString() == "Blue") {
+                    frontState = ConveyorState.BLUE;
+                } else if (colorSensor.getColorString() == "Red") {
+                    frontState = ConveyorState.RED;
+                } else {
+                    frontState = ConveyorState.NONE;
+                }
+            }
+        }
     }
 
     @Override
@@ -55,5 +81,13 @@ public class ConveyorAutomation extends CommandBase {
     @Override
     public boolean isFinished() {
         return false;
+    }
+
+    public ConveyorState getFront() {
+        return frontState;
+    }
+
+    public ConveyorState getBack() {
+        return backState;
     }
 }
