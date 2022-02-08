0c49b4a52fba2478148f96d05cbb75b7f56ea6b4
Josiah Crossman <josiah.crossman@gmail.com>
2022-02-07 17:25:50 -0800

    More Autonomous shooter code
    idk what you want from me here...

--- src/main/java/frc/robot/commands/shooter/FullAutoShooterAssembly.java
+++ src/main/java/frc/robot/commands/shooter/FullAutoShooterAssembly.java
@@ -2,18 +2,12 @@ package frc.robot.commands.shooter;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.TableEntryListener;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.devices.Lemonlight;
 import frc.robot.subsystems.Conveyor;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Shooter;
-
-
+import frc.robot.devices.Lemonlight;
 import java.lang.Math;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import edu.wpi.first.wpilibj.DriverStation;
 import frc.robot.subsystems.Conveyor.ConveyorState;
 
@@ -23,24 +17,23 @@ import frc.robot.subsystems.Conveyor.ConveyorState;
  * Command for running the shooter in full auto mode.
  */
 public class FullAutoShooterAssembly extends CommandBase {
+    // Variables for various things, names are pretty explanitory
     private Shooter shooter;
     private Conveyor conveyor;
     private ConveyorState teamClrIsBlue;
     private Drivetrain drivetrain;
-    // other variables
     private NetworkTable hoodTable;
     private NetworkTable speedTable;
-    private NetworkTableEntry tx;
     private double distance;
     private NetworkTableEntry speed;
     private NetworkTableEntry hood;
     private boolean hoodAngle;
     private double motorPower;
-    private NetworkTable limTable;
-    // devices
-    private Lemonlight limelight;
-
-
+    private double motorSpeed;
+    private ConveyorState indexState;
+    private boolean isBallIndexed;
+    private ShooterStates motorState;
+    private ShooterStates hoodState;
 
     /**
      * Command for running the shooter in full auto mode.
@@ -48,26 +41,26 @@ public class FullAutoShooterAssembly extends CommandBase {
      * @param shooter The shooter subsystem.
      * @param conveyor The conveyor subsystem.
      */
-    public FullAutoShooterAssembly(Shooter shooter,
-        Conveyor conveyor,
-        Drivetrain drivetrain,
-        Lemonlight limelight) {
+    public FullAutoShooterAssembly(Shooter shooter, Conveyor conveyor, Drivetrain drivetrain) {
         this.shooter = shooter;
         this.conveyor = conveyor;
         this.drivetrain = drivetrain;
         addRequirements(shooter);
     }
-    // constants
-    private final double TARGET_MOTOR_SPEED = 0;
+    // Enum for setting state
+    public enum ShooterStates{
+        IDLE,
+        REVVING,
+        READY,
+        MISALIGNED, 
+        UNKNOWN
+    }
 
     // devices
     private Lemonlight limelight = new Lemonlight("Shooter");
 
     /**
-     * Finds the distance between the limelight and the target.
-     *
-     * @param tx The angle, in degrees of the camera.
-     * @return distance The distance between the limelight and the target, in feet.
+     * Initializing variables
      */
     @Override
     public void initialize(){
@@ -79,10 +72,12 @@ public class FullAutoShooterAssembly extends CommandBase {
         }else{
             teamClrIsBlue = ConveyorState.BLUE;
         }
-        
+        motorState = ShooterStates.IDLE;
+        hoodState = ShooterStates.UNKNOWN;
     }
     @Override
-    public void execute() {        
+    public void execute() { 
+        // Checking Variable       
         motorSpeed = shooter.getShooterVelocity();
         distance = limelight.getLimelightDistanceEstimateIN();
         distance = Math.round(distance);
@@ -92,12 +87,24 @@ public class FullAutoShooterAssembly extends CommandBase {
         motorPower = speed.getDouble(0);
         indexState = conveyor.getWillBeIndexedState();
         isBallIndexed = conveyor.getIsBallIndexed();
-        if (isBallIndexed && indexState == teamClrIsBlue)
-        if (motorSpeed != motorPower){
-            shooter.setMotorTargetSpeed(motorPower);
-        }
-        if (hoodAngle != shooter.getHoodPos()) {
-            shooter.setHoodPos(hoodAngle);
+        //Checking to make sure we have a ball and it is the same color as our team
+        if (isBallIndexed && indexState == teamClrIsBlue){
+            if (motorSpeed != motorPower){
+                shooter.setMotorTargetSpeed(motorPower);
+                motorState = ShooterStates.REVVING;
+            } else{
+                motorState = ShooterStates.READY;
+            }
+            if (hoodAngle != shooter.getHoodPos()) {
+                shooter.setHoodPos(hoodAngle);
+                hoodState = ShooterStates.MISALIGNED;
+            }else{
+                hoodState = ShooterStates.READY;
+            }
+            if (motorState == ShooterStates.READY && hoodState == ShooterStates.READY){
+                //TODO Add Code to actually shoot, check offset
+            }
+
         }
         
         
