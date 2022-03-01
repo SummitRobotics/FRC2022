41bd61e33387ae582461ebbda8f99c9eb7e66522
Eric Verheyden <erve2005@gmail.com>
2022-02-28 16:12:43 -0800

    setuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,7 +3,7 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
-import java.util.function.Supplier;
+
 import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.RobotBase;
@@ -15,21 +15,18 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.PneumaticTestCommand;
+import frc.robot.commands.climb.ClimbAutomation;
 import frc.robot.commands.climb.ClimbMO;
 import frc.robot.commands.conveyor.ConveyorAutomation;
 import frc.robot.commands.conveyor.ConveyorMO;
 import frc.robot.commands.drivetrain.ArcadeDrive;
 import frc.robot.commands.intake.DefaultIntake;
-import frc.robot.commands.intake.IntakeMO;
-import frc.robot.commands.climb.ClimbAutomation;
-import frc.robot.commands.conveyor.ConveyorAutomation;
-import frc.robot.commands.conveyor.ConveyorMO;
-import frc.robot.commands.drivetrain.ArcadeDrive;
 import frc.robot.commands.intake.FullAutoIntake;
+import frc.robot.commands.intake.IntakeMO;
 import frc.robot.commands.intake.IntakeToggle;
-import frc.robot.commands.shooter.ShooterMO;
 import frc.robot.commands.shooter.FullAutoShooterAssembly;
-import frc.robot.utilities.Functions;
+import frc.robot.commands.shooter.ShooterMO;
+import frc.robot.devices.ColorSensor;
 import frc.robot.devices.LEDs.LEDCall;
 import frc.robot.devices.LEDs.LEDRange;
 import frc.robot.devices.LEDs.LEDs;
@@ -49,15 +46,20 @@ import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.Intake;
 import frc.robot.subsystems.Shooter;
 import frc.robot.utilities.ChangeRateLimiter;
+import frc.robot.utilities.Functions;
 import frc.robot.utilities.lists.Colors;
 import frc.robot.utilities.lists.LEDPriorities;
 import frc.robot.utilities.lists.Ports;
 import frc.robot.utilities.lists.StatusPriorities;
+import java.util.function.Supplier;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
@@ -78,12 +80,9 @@ public class RobotContainer {
     private final PDP pdp;
     private final PCM pcm;
     private final AHRS gyro;
+
     private final ColorSensor colorSensor;
     private final LidarV4 lidar;
-
-    
-    //private final ColorSensor colorSensor;
-    //private final LidarV3 lidarV3;
     private Command fullAutoShooterAssembly;
     private Supplier<Command> fullAutoIntake;
     private final Command teleInit;
@@ -106,7 +105,7 @@ public class RobotContainer {
 
         new LEDCall("disabled", LEDPriorities.ON, LEDRange.All).solid(Colors.DIM_GREEN).activate();
         ShuffleboardDriver.statusDisplay.addStatus(
-            "default", "robot on", Colors.WHITE, StatusPriorities.ON);
+                "default", "robot on", Colors.WHITE, StatusPriorities.ON);
 
         gyro = new AHRS();
         targetingLimelight = new Lemonlight("limelight");
@@ -120,16 +119,16 @@ public class RobotContainer {
         intake = new Intake();
         climb = new Climb(gyro);
         fullAutoShooterAssembly = new ParallelCommandGroup(
-            new FullAutoShooterAssembly(shooter, conveyor, drivetrain, targetingLimelight),
-            new ConveyorAutomation(conveyor));
+                new FullAutoShooterAssembly(shooter, conveyor, drivetrain, targetingLimelight),
+                new ConveyorAutomation(conveyor, intake));
         fullAutoIntake = () -> new FullAutoIntake(drivetrain, ballDetectionLimelight);
         autoInit = new SequentialCommandGroup(
                 new InstantCommand(
                         () -> ShuffleboardDriver.statusDisplay.addStatus(
-                            "auto",
-                            "robot in auto",
-                            Colors.TEAM,
-                            StatusPriorities.ENABLED)),
+                                "auto",
+                                "robot in auto",
+                                Colors.TEAM,
+                                StatusPriorities.ENABLED)),
                 new InstantCommand(drivetrain::highGear),
                 new InstantCommand(intake::lowerIntake),
                 new InstantCommand(() -> {
@@ -137,43 +136,41 @@ public class RobotContainer {
                     launchpad.bigLEDGreen.set(true);
                 }),
                 new InstantCommand(climb::zeroClimb));
-                
 
-        teleInit =
-            new SequentialCommandGroup(
+        teleInit = new SequentialCommandGroup(
                 new InstantCommand(
-                    () -> {
-                        // simulated robots don't have joysticks
-                        if (RobotBase.isReal()) {
-                            if (!controller1.isConnected()
-                                || !launchpad.isConnected()
-                                || !joystick.isConnected()
-                            ) {
-                                System.out.println(
-                                    "not enough joysticks connected,"
-                                        + "please make sure the xbox controller,launchpad,"
-                                        + "and joystick are connected to the driver-station");
-                                throw (new RuntimeException("not enough joysticks connected"));
+                        () -> {
+                            // simulated robots don't have joysticks
+                            if (RobotBase.isReal()) {
+                                if (!controller1.isConnected()
+                                        || !launchpad.isConnected()
+                                        || !joystick.isConnected()) {
+                                    System.out.println(
+                                            "not enough joysticks connected,"
+                                                    + "please make sure the xbox controller,launchpad,"
+                                                    + "and joystick are connected to the driver-station");
+                                    throw (new RuntimeException("not enough joysticks connected"));
+                                }
+
+                                if (!controller1.isXboxController()) {
+                                    System.out.println("controller 0 is not the xbox controller");
+                                    throw (new RuntimeException("incorrect joystick in port 0"));
+                                }
                             }
-                            
-                            if (!controller1.isXboxController()) {
-                                System.out.println("controller 0 is not the xbox controller");
-                                throw (new RuntimeException("incorrect joystick in port 0"));
-                            }
-                        }
-                    }),
+                        }),
                 new InstantCommand(() -> pcm.enableCompressorDigital()),
                 new InstantCommand(() -> ShuffleboardDriver.statusDisplay.removeStatus("auto")),
                 new InstantCommand(
                         () -> ShuffleboardDriver.statusDisplay.addStatus(
-                            "enabled",
-                            "robot enabled",
-                            Colors.TEAM,
-                            StatusPriorities.ENABLED)),
+                                "enabled",
+                                "robot enabled",
+                                Colors.TEAM,
+                                StatusPriorities.ENABLED)),
                 new InstantCommand(() -> joystick.reEnableJoystickCalibrationCheck()),
                 new InstantCommand(drivetrain::highGear),
                 // new InstantCommand(() -> targetingLimelight.setLEDMode(LEDModes.FORCE_OFF)),
-                // new InstantCommand(() -> ballDetectionLimelight.setLEDMode(LEDModes.FORCE_OFF)),
+                // new InstantCommand(() ->
+                // ballDetectionLimelight.setLEDMode(LEDModes.FORCE_OFF)),
                 new InstantCommand(() -> {
                     launchpad.bigLEDRed.set(false);
                     launchpad.bigLEDGreen.set(true);
@@ -197,19 +194,21 @@ public class RobotContainer {
     private void setDefaultCommands() {
         // drive by controller
         drivetrain.setDefaultCommand(new ArcadeDrive(
-            drivetrain,
-            controller1.rightTrigger,
-            controller1.leftTrigger,
-            controller1.leftX));
+                drivetrain,
+                controller1.rightTrigger,
+                controller1.leftTrigger,
+                controller1.leftX));
         // intake.setDefaultCommand(new DefaultIntake(intake, conveyor));
         shooter.setDefaultCommand(new ShooterMO(shooter, joystick.axisZ, launchpad.buttonC));
-        //conveyor.setDefaultCommand(new ConveyorAutomation(conveyor, intake));
+        // conveyor.setDefaultCommand(new ConveyorAutomation(conveyor, intake));
     }
 
     /**
-     * Use this method to define your button->command mappings. Buttons can be created by
+     * Use this method to define your button->command mappings. Buttons can be
+     * created by
      * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
+     * it to a {@link
      * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
      */
     private void configureButtonBindings() {
@@ -219,23 +218,24 @@ public class RobotContainer {
         // MOs
         launchpad.buttonB.whileHeld(new ConveyorMO(conveyor, joystick.axisY, joystick.button2, joystick.button3));
         launchpad.buttonA.whileHeld(new IntakeMO(intake, joystick.axisY, joystick.button2));
-        launchpad.missileB.whileHeld(new ClimbMO(climb, joystick.axisY, joystick.button2, joystick.button3, launchpad.buttonD, launchpad.buttonI, launchpad.buttonH, launchpad.buttonG));
+        launchpad.missileB.whileHeld(new ClimbMO(climb, joystick.axisY, joystick.button2, joystick.button3,
+                launchpad.buttonD, launchpad.buttonI, launchpad.buttonH, launchpad.buttonG));
     }
 
     /**
      * Use this method to init all the subsystems' telemetry stuff.
      */
     private void initTelemetry() {
-        //SmartDashboard.putData("PDP", pdp);
-        //SmartDashboard.putData("PCM", pcm);
-        //SmartDashboard.putData("Drivetrain", drivetrain);
+        // SmartDashboard.putData("PDP", pdp);
+        // SmartDashboard.putData("PCM", pcm);
+        // SmartDashboard.putData("Drivetrain", drivetrain);
         // SmartDashboard.putData("Lemonlight", targetingLimelight);
         // SmartDashboard.putData("Lemonlight", ballDetectionLimelight);
         SmartDashboard.putData("Shooter", shooter);
-        //SmartDashboard.putData("Conveyor", conveyor);
-        //SmartDashboard.putData("Intake", intake);
-        //SmartDashboard.putData("Color Sensor", colorSensor);
-        //SmartDashboard.putData("Lidar", lidar);
+        // SmartDashboard.putData("Conveyor", conveyor);
+        // SmartDashboard.putData("Intake", intake);
+        // SmartDashboard.putData("Color Sensor", colorSensor);
+        // SmartDashboard.putData("Lidar", lidar);
     }
 
     /**
@@ -253,29 +253,28 @@ public class RobotContainer {
      */
     public void robotInit() {
         ShuffleboardDriver.init();
-        //sets up all the splines so we dont need to spend lots of time
-        //turning the json files into trajectorys when we want to run them
-        //TODO make this run faster somehow
+        // sets up all the splines so we dont need to spend lots of time
+        // turning the json files into trajectorys when we want to run them
+        // TODO make this run faster somehow
         String ball1 = "paths\1stBlue.path";
         try {
             Command fball1 = Functions.splineCommandFromFile(drivetrain, ball1);
-            //possible 4 ball auto
+            // possible 4 ball auto
             auto = new SequentialCommandGroup(
-                autoInit,
-                fball1, 
-                fullAutoShooterAssembly,
-                fullAutoIntake.get(),
-                fullAutoShooterAssembly,
-                fullAutoIntake.get(),
-                fullAutoShooterAssembly
-            );
-        
-        }catch (Exception e){
+                    autoInit,
+                    fball1,
+                    fullAutoShooterAssembly,
+                    fullAutoIntake.get(),
+                    fullAutoShooterAssembly,
+                    fullAutoIntake.get(),
+                    fullAutoShooterAssembly);
+
+        } catch (Exception e) {
             System.out.println("An error occured when making autoInit: " + e);
         }
 
     }
-    
+
     /**
      * runs once every ~20ms when in teleop.
      */
