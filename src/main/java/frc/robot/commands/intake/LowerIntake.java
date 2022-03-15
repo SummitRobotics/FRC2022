f39dbae613663daf124a25ef7fdf6b4a1dc09642
silicon <cheesychezbread@gmail.com>
2022-03-15 16:53:50 -0700

    Auto stash before merge of "LEDs" and "testing"

--- src/main/java/frc/robot/commands/intake/LowerIntake.java
+++ src/main/java/frc/robot/commands/intake/LowerIntake.java
@@ -1,7 +1,12 @@
 package frc.robot.commands.intake;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.devices.LEDs.LEDCall;
+import frc.robot.devices.LEDs.LEDRange;
+import frc.robot.devices.LEDs.LEDs;
 import frc.robot.subsystems.Intake;
+import frc.robot.utilities.lists.Colors;
+import frc.robot.utilities.lists.LEDPriorities;
 
 /**
  * Command for lowering the intake.
@@ -17,6 +22,7 @@ public class LowerIntake extends InstantCommand {
 
     @Override
     public void initialize() {
+        LEDs.getInstance().addCall("Intake Down", new LEDCall(LEDPriorities.INTAKE_DOWN, LEDRange.All).ffh(Colors.BLUE, Colors.OFF));
         intake.setIntakeSolenoid(true);
         intake.setIntakeMotorPower(Intake.INTAKE_MOTOR_SPEED);
     }
