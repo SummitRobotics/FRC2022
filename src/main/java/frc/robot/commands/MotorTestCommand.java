f99792aa90dc2623e95bd41c1892ad2529f80d2e
Owen McCormick <oweninoregon@gmail.com>
2022-03-17 08:15:08 -0700

    Set check to stop motors when each are finished

--- src/main/java/frc/robot/commands/MotorTestCommand.java
+++ src/main/java/frc/robot/commands/MotorTestCommand.java
@@ -98,34 +98,42 @@ public class MotorTestCommand extends CommandBase {
     @Override
     public void execute() {
         if (shooter.getEncoderValue() > shooterEncoderPos + 0.5) {
+            shooter.stop();
             shooterIsGood = true;
         }
 
         if (intake.getIntakeEncoderPosition() > intakeEncoderPos + 0.5) {
+            intake.stop();
             intakeIsGood = true;
         }
 
         if (drivetrain.getLeftEncoderPosition() > leftDrivetrainEncoderPos + 0.5) {
+            drivetrain.setLeftMotorPower(0);
             leftDrivetrainIsGood = true;
         }
 
         if (drivetrain.getRightEncoderPosition() > rightDrivetrainEncoderPos + 0.5) {
+            drivetrain.setRightMotorPower(0);
             rightDrivetrainIsGood = true;
         }
 
         if (conveyor.getIndexEncoderPosition() > indexConveyorEncoderPos + 0.5) {
+            conveyor.setIndexMotorPower(0);
             indexConveyorIsGood = true;
         }
 
         if (conveyor.getBeltEncoderPosition() > beltConveyorEncoderPos + 0.5) {
+            conveyor.setBeltMotorPower(0);
             beltConveyorIsGood = true;
         }
 
         if (climb.getLeftEncoderValue() > leftClimbEncoderPos + 0.5) {
+            climb.setLeftMotorPower(0);
             leftClimbIsGood = true;
         }
 
         if (climb.getRightEncoderValue() > rightClimbEncoderPos + 0.5) {
+            climb.setRightMotorPower(0);
             rightClimbIsGood = true;
         }
 
@@ -134,11 +142,6 @@ public class MotorTestCommand extends CommandBase {
     @Override
     public void end(final boolean interrupted) {
         timer.stop();
-        shooter.stop();
-        intake.stop();
-        drivetrain.stop();
-        conveyor.stop();
-        climb.stop();
     }
 
     @Override
